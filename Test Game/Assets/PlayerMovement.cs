ea5571d1f89be38a451b60d39308c24838f10f26
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2018-11-14 16:35:48 -0500

    Tutorial 02
    Completed Part 2 of the tutorial

---
+++ Test Game/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        // Use "Debug.Log()" to print
+        // Debug.Log("Hello, World!");
+        // rb.useGravity = false;
+        // rb.AddForce(0, 200, 500);
+	}
+	
+	// Update is called once per frame
+    // Use FixedUpdate for physics in Unity
+    // Use Time.deltaTime in order to make speeds uniform across all omputers
+	void FixedUpdate () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+}
