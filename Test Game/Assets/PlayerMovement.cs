e06516eafdcf007c7ba094ed1ad617ee50271037
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2018-11-14 16:52:45 -0500

    Tutorial 03
    Completed Part 3 of the tutorial

--- Test Game/Assets/PlayerMovement.cs
+++ Test Game/Assets/PlayerMovement.cs
@@ -3,19 +3,46 @@
 public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
+    // Good practice to put "f" after floating point numbers in Unity
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    private bool moveRight;
+    private bool moveLeft;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
         // Use "Debug.Log()" to print
         // Debug.Log("Hello, World!");
         // rb.useGravity = false;
         // rb.AddForce(0, 200, 500);
 	}
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKey("d")) {
+            moveRight = true;
+        } else {
+            moveRight = false;
+        }
+
+        if(Input.GetKey("a")) {
+            moveLeft = true;
+        } else {
+            moveLeft = false;
+        }
+    }
     // Use FixedUpdate for physics in Unity
     // Use Time.deltaTime in order to make speeds uniform across all omputers
-	void FixedUpdate () {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(moveRight) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if(moveLeft) {
+            rb.AddForce(-1 * sidewaysForce * Time.deltaTime, 0, 0);
+        }
 	}
 }
