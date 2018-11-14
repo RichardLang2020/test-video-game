50ea956a06ab3100874530fb2188c3b75a4038f2
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2018-11-14 17:01:17 -0500

    Tutorial 04
    Completed Part 4 of the tutorial

---
+++ Test Game/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    // Vector3 stores 3 floats into a vector format
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log(player.position);
+        transform.position = player.position + offset;
+	}
+}
