3f3c15b85cae1e7bac5dba054f234210189cee4c
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2018-11-16 15:56:58 -0500

    Tutorial 05
    Completed Part 5 of the tutorial

---
+++ Test Game/Assets/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collisionInfo) {
+        // Debug.Log(collisionInfo.collider.name);
+
+        if(collisionInfo.collider.tag == "Obstacle") {
+            // Debug.Log("We hit an obstacle!");
+            movement.enabled = false;
+        }
+    }
+}
